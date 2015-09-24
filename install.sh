6aaa3066df56501265bd3e8dda6a33dea33773ab
Jeff Boes <jeff@endpoint.com>
2015-09-24 15:39:23 -0400

    Full install

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
-ln "~/dotfiles/gitconfig" ~/.gitconfig
-ln "~/dotfiles/emacs" ~/.emacs
+ln -fsv "$HOME/dotfiles/gitconfig" ~/.gitconfig
+ln -fsv "$HOME/dotfiles/emacs" ~/.emacs
+grep -qi "dotfiles installed" ~/.bashrc
+if [ $? != 0 ]
+then
+  cat >>~/.bashrc <<EOF
+# dotfiles installed:
+for DOTFILE in `find \$HOME/dotfiles/bash`
+do
+  [ -f “\$DOTFILE” ] && source “\$DOTFILE”
+done
+EOF
+fi
